07f9c74be6545b2b1163e518af4d03cdae1c18c0
Francis Wedemeyer <francisdayagro24@gmail.com>
2024-05-02 04:42:52 +0800

    finished menu but will add more soon

--- app/src/main/java/com/example/androidprojectcollection/MenuExercise.java
+++ app/src/main/java/com/example/androidprojectcollection/MenuExercise.java
@@ -5,10 +5,12 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -16,6 +18,8 @@ public class MenuExercise extends AppCompatActivity {
 
     Button btnChanger;
     int defaultColor; // Variable to store the default color
+    int originalWidth, originalHeight; // Variables to store the original width and height
+    Drawable originalBackground; // Variable to store the original background drawable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,21 @@ public class MenuExercise extends AppCompatActivity {
 
         // Store the default color
         defaultColor = getResources().getColor(android.R.color.black);
+        originalBackground = btnChanger.getBackground();
+
+
+        // Add a layout listener to capture the original size after layout
+        btnChanger.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // Retrieve the original width and height
+                originalWidth = btnChanger.getWidth();
+                originalHeight = btnChanger.getHeight();
+
+                // Remove the listener to avoid multiple calls
+                btnChanger.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+        });
 
         // Set initial click listener for the button
         btnChanger.setOnClickListener(new View.OnClickListener() {
@@ -50,8 +69,22 @@ public class MenuExercise extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.mItemChangeColor:
-                // Change color to red
-                btnChanger.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_red_dark)));
+                // Cycle through different colors
+                int[] colors = {
+                        android.R.color.holo_red_dark,
+                        android.R.color.holo_orange_dark,
+                        android.R.color.holo_blue_bright
+                };
+                int currentColor = btnChanger.getBackgroundTintList().getDefaultColor();
+                int nextColor = colors[0]; // Default to the first color
+                for (int i = 0; i < colors.length; i++) {
+                    if (currentColor == getResources().getColor(colors[i])) {
+                        // Get the next color in the array
+                        nextColor = colors[(i + 1) % colors.length];
+                        break;
+                    }
+                }
+                btnChanger.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(nextColor)));
                 return true;
 
             case R.id.mItemChangeSize:
@@ -68,6 +101,11 @@ public class MenuExercise extends AppCompatActivity {
                 constraintSet.centerVertically(R.id.btnTransform, ConstraintSet.PARENT_ID);
                 constraintSet.applyTo((ConstraintLayout) findViewById(R.id.conslayout)); // Replace conslayout with the actual ID of your ConstraintLayout
 
+                return true;
+
+            case R.id.mItemChangeSquare:
+                // Change shape to square
+                btnChanger.setBackgroundResource(R.drawable.square);
                 return true;
 
 
@@ -87,11 +125,25 @@ public class MenuExercise extends AppCompatActivity {
 
             case R.id.mItemReset:
                 // Reset to default appearance
+                btnChanger.setBackground(originalBackground); // Reset background to original
                 btnChanger.setBackgroundTintList(ColorStateList.valueOf(defaultColor)); // Reset color to default
                 btnChanger.setText("Button"); // Reset text to default
                 btnChanger.setVisibility(View.VISIBLE); // Reset visibility to default
-                return true;
 
+                // Reset size to original dimensions
+                ConstraintLayout.LayoutParams defaultParams = (ConstraintLayout.LayoutParams) btnChanger.getLayoutParams();
+                defaultParams.width = originalWidth;
+                defaultParams.height = originalHeight;
+                btnChanger.setLayoutParams(defaultParams);
+
+                // Set constraints to keep the button centered horizontally and vertically
+                ConstraintSet resetConstraintSet = new ConstraintSet();
+                resetConstraintSet.clone((ConstraintLayout) findViewById(R.id.conslayout)); // Replace conslayout with the actual ID of your ConstraintLayout
+                resetConstraintSet.centerHorizontally(R.id.btnTransform, ConstraintSet.PARENT_ID);
+                resetConstraintSet.centerVertically(R.id.btnTransform, ConstraintSet.PARENT_ID);
+                resetConstraintSet.applyTo((ConstraintLayout) findViewById(R.id.conslayout)); // Replace conslayout with the actual ID of your ConstraintLayout
+
+                return true;
 
             case R.id.mItemExit:
                 finish();
